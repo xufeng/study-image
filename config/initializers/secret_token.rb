a39bd1a83802f4536fbfb63ac0df7ddec936be64
xufeng <81959804@qq.com>
2012-02-05 22:00:55 +0800

    处理图片

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Images::Application.config.secret_token = '8d8d284ca6efa65c1cc2d1ba1ce1bd48069f7a8c6e974285b6a1d5c46553c63efff98a6056eefbad8c4b79d4914cd822dc5b91b12d6e2b1392777d6ed80d0cfa'
